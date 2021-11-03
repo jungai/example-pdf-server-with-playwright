d34772e8d37eb8e4972932701fedd39b9b31026f
jungai <juworaphol125@gmail.com>
2021-11-03 16:14:41 +0700

    refactor(utils): export streamToString

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,2 +1,3 @@
 export { s3Client, getObjectInS3 } from './s3';
 export { getAwsAccessKeyId, getBucketName, getAwsSecretAccessKey } from './get_env';
+export { streamToString } from './stream_to_string';
