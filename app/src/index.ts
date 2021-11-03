6f7a381593ab0d60eb178358bc52b3468439f0c5
jungai <juworaphol125@gmail.com>
2021-11-03 19:32:19 +0700

    feat: add example pipeline

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello');
+import 'dotenv/config';
+import { Readable } from 'stream';
+import { getBucketName, getObjectInS3, s3Client } from './utils';
+import { streamToString } from './utils/stream_to_string';
+import { from, mergeMap, tap } from 'rxjs';
+import { generatePdf } from './utils/pdf';
+
+(() => {
+    const pdfBuffer = from(
+        s3Client.send(
+            getObjectInS3({
+                Bucket: getBucketName(),
+                Key: 'index.html', // test eiei
+                ResponseContentType: 'application/html',
+            }),
+        ),
+    ).pipe(
+        tap((s3Output) => {
+            if (!s3Output) {
+                throw new Error('no s3 output!!');
+            }
+        }),
+        mergeMap((s3FileOutput) => from(streamToString(s3FileOutput.Body as Readable))),
+        mergeMap((html) => from(generatePdf(html))),
+    );
+
+    pdfBuffer.subscribe({
+        next() {
+            console.log('done');
+        },
+    });
+})();
